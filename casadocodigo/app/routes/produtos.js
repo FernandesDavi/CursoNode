35785ecd62c753e05bdf0c943b8d0330129fae19
Davi da Silva Fernades <davi.fernandes2012@gmail.com>
2018-05-11 13:51:44 -0300

    dia 2

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,6 +1,21 @@
-module.exports = function(app){
-app.get('/produtos',function(req,res){
-    var mysql = require('mysql');
-    res.render("produtos/lista");
-});
+module.exports = function (app) {
+    app.get('/produtos', function (req, res) {
+        var connection = app.infra.connectionFactory();
+        var produtosBanco = new app.infra.produtosBanco(connection);
+
+        produtosBanco.lista(function (err, results) {
+            res.render('produtos/lista', { lista: results });
+        });
+
+        connection.end();
+    });
+    app.get('/produtos/remove', function () {
+        var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco(connection);
+        var produto = produtosBanco.carrega(id, callback);
+        if (produto) {
+            produtosBanco.remove(produto, callback);
+        }
+    });
+
 }
