d896276192d61976b6ea720205c68be0e166aa3e
Davi da Silva Fernades <davi.fernandes2012@gmail.com>
2018-05-14 15:52:24 -0300

    123

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,21 +1,28 @@
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.produtosBanco(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function (err, results) {
+        produtosDAO.lista(function (err, results) {
             res.render('produtos/lista', { lista: results });
         });
 
         connection.end();
     });
-    app.get('/produtos/remove', function () {
-        var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco(connection);
-        var produto = produtosBanco.carrega(id, callback);
-        if (produto) {
-            produtosBanco.remove(produto, callback);
-        }
+    app.get('/produtos/form', function (req, res) {
+
+        res.render('produtos/form');
+
     });
 
+    app.post('/produtos/salva', function (req, res) {
+        var produto = req.body;
+        console.log(produto);
+         var connection = app.infra.connectionFactory();
+         var produtosDAO = new app.infra.ProdutosDAO(connection);
+         produtosDAO.salva(produto, function (err, results) {
+             res.render('produtos/lista');
+
+         });
+    });
 }
--- produtosWeb.js
+++ produtosWeb.js
@@ -8,6 +8,6 @@ http.createServer(function(req,res){
         res.end("<html><body>Home da casa do Codigo</body></html>");
     }
    }).listen(3000,"localhost");
-
+ 
 
 console.log("Servidor On");
