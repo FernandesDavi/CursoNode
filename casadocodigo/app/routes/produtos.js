e3ebfc3013d8548126d52ada22357dc73c059cd5
Davi da Silva Fernades <davi.fernandes2012@gmail.com>
2018-05-15 15:41:31 -0300

    dia 15

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,28 +1,44 @@
 module.exports = function (app) {
+
     app.get('/produtos', function (req, res) {
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
         produtosDAO.lista(function (err, results) {
-            res.render('produtos/lista', { lista: results });
+            res.format({
+                html: function () {
+                    res.render('produtos/lista', { lista: results });
+
+                },
+                json: function () {
+                    res.json(results);
+                }
+            });
         });
 
         connection.end();
     });
+
     app.get('/produtos/form', function (req, res) {
 
-        res.render('produtos/form');
+        res.render('produtos/form',{errosValidacao: ''});
 
     });
 
-    app.post('/produtos/salva', function (req, res) {
+    app.post('/produtos', function (req, res) {
         var produto = req.body;
-        console.log(produto);
-         var connection = app.infra.connectionFactory();
-         var produtosDAO = new app.infra.ProdutosDAO(connection);
-         produtosDAO.salva(produto, function (err, results) {
-             res.render('produtos/lista');
 
-         });
+       req.assert('titulo','Titulo é obrigatorio').notEmpty();
+        req.assert('preco','Formato Invalido').isFloat();
+        var erros = req.validationErrors();
+        if(erros){
+            res.render('produtos/form',{errosValidacao: erros});
+            return;
+        }
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function (err, results) {
+            res.redirect('/produtos');
+        });
     });
 }
---
+++ casadocodigo/cadastra-livrosTerminal.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var config = {
+    hostname: 'localhost',
+    port:3000,
+    path:'/produtos',
+    method: 'post',
+    headers:{
+        'Accept':'application/json',
+        'Content-type':'application/json'
+    }
+};
+var cliente = http.request(config,function(res){
+    console.log(res.statusCode)
+    res.on('data',function(body){
+        console.log('Corpo:'+body);
+    })
+});
+var produto = {
+    titulo: 'mais sobre node',
+    descricao: 'node JS',
+    preco: 100
+}
+cliente.end(JSON.stringify(produto));
